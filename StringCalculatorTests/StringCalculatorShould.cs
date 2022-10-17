3d1910682a5ac497a05a450904ce5ba9930f36db
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-17 08:27:13 +0100

    Agregar archivos de proyecto.

---
+++ StringCalculatorTests/StringCalculatorShould.cs
@@ -0,0 +1,12 @@
+namespace StringCalculatorTests {
+    public class StringCalculatorShould {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+
+        }
+    }
+}
