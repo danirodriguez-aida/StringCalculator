cfa4bfd26711d49a6dd4bf2a6d54e4d71e8f5970
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-17 08:39:25 +0100

    red test return_one_when_string_only_has_one

--- StringCalculatorTests/StringCalculatorShould.cs
+++ StringCalculatorTests/StringCalculatorShould.cs
@@ -10,10 +10,17 @@ namespace StringCalculatorTests {
         [Test]
         public void return_zero_when_string_empty()
         {
-            
             var result = StringCalculator.Sum("");
 
             result.Should().Be(0);
+        }
+
+        [Test]
+        public void return_one_when_string_only_has_one()
+        {
+            var result = StringCalculator.Sum("1");
+
+            result.Should().Be(1);
         }
     }
 }
