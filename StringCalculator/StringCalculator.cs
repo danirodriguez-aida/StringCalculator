0be9840c6630404c7394c09d443cbff13ab68d7e
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-17 08:55:32 +0100

    green test return_the_sum_of_two_numbers_when_these_are_separated_by_comma

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -2,9 +2,13 @@ namespace StringCalculatorProject;
 
 public static class StringCalculator
 {
-    public static float Sum(string values)
-    {
+    public static float Sum(string values) {
         if (string.IsNullOrWhiteSpace(values)) return 0;
+        var numbers = values.Split(",");
+        if (numbers.Length == 2)
+        {
+            return Convert.ToSingle(numbers[0]) + Convert.ToSingle(numbers[1]);
+        }
         return Convert.ToSingle(values);
     }
 }
