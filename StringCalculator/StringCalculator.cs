2f0219cb6d824b1f8f949b66aa0a86d2bd381bc4
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-18 07:05:24 +0100

    green test return_the_sum_of_more_than_two_numbers_separated_by_comma

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -5,10 +5,6 @@ public static class StringCalculator
     public static int Add(string values) {
         if (string.IsNullOrWhiteSpace(values)) return 0;
         var numbers = values.Split(",");
-        if (numbers.Length == 2)
-        {
-            return Convert.ToInt32(numbers[0]) + Convert.ToInt32(numbers[1]);
-        }
-        return Convert.ToInt32(values);
+        return numbers.Sum(Convert.ToInt32);
     }
 }
